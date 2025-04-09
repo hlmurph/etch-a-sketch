4d752f13fa9f22db14ba3a8ae70a8ccaec29c72a
hlmurph <106940356+hlmurph@users.noreply.github.com>
2025-04-09 18:23:45 -0400

    Add logic to iterate and create correct number of divs

---
+++ script.js
@@ -0,0 +1,7 @@
+let numberOfSquares = 16;
+let container = document.querySelector('#container');
+
+for (let i=0; i < numberOfSquares; i++) {
+    let div = document.createElement('div');
+    container.appendChild(div);
+};
