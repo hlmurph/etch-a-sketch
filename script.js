f06af9676fefa12a01fbf9abc8be537e16ddeda0
hlmurph <106940356+hlmurph@users.noreply.github.com>
2025-04-09 18:49:31 -0400

    Make the div's arrange in a grid

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 let numberOfSquares = 16;
 let container = document.querySelector('#container');
 
-for (let i=0; i < numberOfSquares; i++) {
-    let div = document.createElement('div');
-    container.appendChild(div);
-};
+let rows = Math.sqrt(numberOfSquares);
+
+for (let i = 0; i < rows; i++) {
+    let row = document.createElement('div');
+    row.setAttribute('id', 'row');
+    container.appendChild(row);
+    for (let i=0; i < rows; i++) {
+            let div = document.createElement('div');
+            div.setAttribute('id', 'flex-item');
+            row.appendChild(div);
+        };
+}
